082bc56ab11bd260aa2cf98f4977d3eefa66a005
Andrey <andrejverkeev99@gmail.com>
2026-08-12 00:30:52 +0300

    style: add global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import App from "./App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/index.scss";
+import "./styles/reset.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
